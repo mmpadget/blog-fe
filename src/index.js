031b2ed26816365f29a91bbf448b2f77ded1077f
Michael Padget <mpadget@comcast.net>
2018-12-03 14:43:11 -0800

    Dispatch checkbox action

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from 'redux'
 
@@ -19,13 +19,35 @@ const reducer = function(state = defaultState, action) {
 const store = createStore(reducer)
 console.log(store)
 
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <h1>Hello, World!</h1>
-//     )
-//   }
-// }
+class App extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  componentWillMount() {
+    store.subscribe(() => this.setState(store.getState()))
+  }
+
+  render() {
+    const onClick = () => store.dispatch({ type: 'TOGGLE' })
+    return (
+      <div>
+        <h1>To-dos</h1>
+        <div>
+          Learn Redux &nbsp;
+          <input
+            type="checkbox"
+            checked={!!this.state.checked}
+            onClick={onClick} />
+        </div>
+        {
+          this.state.checked ? (<h2>Done!</h2>) : null
+        }
+      </div>
+    )
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
