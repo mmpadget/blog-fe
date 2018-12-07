f2ec5ea7962aa33c50472acbd875e4cd605a0268
Michael Padget <mpadget@comcast.net>
2018-12-06 16:41:23 -0800

    Middleware for custom HTTP requests

--- src/agent.js
+++ src/agent.js
@@ -1,9 +1,5 @@
 import superagent from 'superagent'
-//import _superagent from 'superagent'
-// import superagentPromise from 'superagent-promise'
 
-// const superagentPromise(_superagent, global.Promise)
-// const API_ROOT = 'https://conduit.productionready.io/api'
 const API_ROOT = 'http://localhost:3000'
 
 const responseBody = res => res.body
@@ -16,7 +12,6 @@ const requests = {
 const Articles = {
   all: page =>
     requests.get(`/articles?limit=10`)
-    // requests.get('/articles?limit=10')
 }
 
 export default {
