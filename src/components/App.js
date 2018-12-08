a2c4e7bc2e6abf52edc2799783a9fc104a69a3d9
Michael Padget <mpadget@comcast.net>
2018-12-07 16:24:04 -0800

    Initial react router setup

--- src/components/App.js
+++ src/components/App.js
@@ -2,21 +2,27 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
 import Header from './Header'
-import Home from './Home'
+// import Home from './Home'
 
 const mapStateToProps = state => ({
   appName: state.appName
 })
 
+// Home component is represented by the props.children property.
 class App extends Component {
   render() {
     return (
       <div>
         <Header appName={this.props.appName} />
-        <Home />
+        {this.props.children}
       </div>
     )
   }
+}
+
+// Tells react-router to attach the children property to this component's props.
+App.contextTypes = {
+  router: React.PropTypes.object.isRequired
 }
 
 export default connect(mapStateToProps, () => ({}))(App)
--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { Router, Route, IndexRoute, hashHistory } from 'react-router'
 
 import App from './components/App'
+import Home from './components/Home'
+import Login from './components/Login'
 import store from './store'
-import * as serviceWorker from './serviceWorker'
 
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render((
   <Provider store={store}>
-    <App />
+    { /* Instantiate a new router.*/ }
+    <Router history={hashHistory}>
+      <Route path="/" component={App}>
+        <IndexRoute component={Home} />
+        <Route path="login" component={Login} />
+      </Route>
+    </Router>
   </Provider>
   ), document.getElementById('root'));
 
