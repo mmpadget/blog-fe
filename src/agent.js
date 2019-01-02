1cd33f3c8a93d1caddd4ef9d932eab83ff2958ba
Michael Padget <mpadget@comcast.net>
2019-01-02 14:03:27 -0800

    Settings component Reset author

--- src/agent.js
+++ src/agent.js
@@ -21,7 +21,9 @@ const requests = {
   get: url =>
     superagent.get(`${API_ROOT}${url}`).use(tokenPlugin).then(responseBody),
   post: (url, body) =>
-    superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
+    superagent.post(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody),
+  put: (url, body) =>
+    superagent.put(`${API_ROOT}${url}`, body).use(tokenPlugin).then(responseBody)
 }
 
 const Articles = {
@@ -36,7 +38,9 @@ const Auth = {
   login: (email, password) =>
     requests.post('/users/login', { user: { email, password } }),
   register: (username, email, password) =>
-    requests.post('/users', { user: { username, email, password } })
+    requests.post('/users', { user: { username, email, password } }),
+  save: user =>
+    requests.put('/user', { user })
 }
 
 // Export a set token function that will take in a token and set the local token variable to the provided token.
--- src/store.js
+++ src/store.js
@@ -4,11 +4,13 @@ import { promiseMiddleware, localStorageMiddleware } from './middleware'
 import auth from './reducers/auth'
 import common from './reducers/common'
 import home from './reducers/home'
+import settings from './reducers/settings'
 
 const reducer = combineReducers({
   auth,
   common,
-  home
+  home,
+  settings
 })
 
 // Use applyMiddleware from redux to create middleware when we create the store.
